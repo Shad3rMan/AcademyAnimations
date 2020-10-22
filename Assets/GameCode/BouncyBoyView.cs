e695f51d643553ff2497f5ed2c821ba1712241d9
nbarkun <nikitaba@playtika.com>
2020-10-22 14:36:40 +0300

    Added new animation

---
+++ Assets/GameCode/BouncyBoyView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace GameCode
+{
+    [RequireComponent(typeof(Animation))] 
+    public class BouncyBoyView : MonoBehaviour
+    {
+        [SerializeField]
+        private ParticleSystem _particleSystem;
+
+        private void PlayLanded()
+        {
+            _particleSystem.Play();
+        }
+    }
+}
