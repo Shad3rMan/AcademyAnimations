83aaf08a911ca3922e0900befa808076c0776dc8
Taras Makarevich <tarasm@playtika.com>
2020-10-22 14:59:53 +0300

    Add VFX on jump start and end.

---
+++ Assets/GameCode/BouncyBoyView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Academy.Animations
+{
+    [RequireComponent(typeof(Animation))]
+    public class BouncyBoyView : MonoBehaviour
+    {
+        [SerializeField]
+        private ParticleSystem _particleSystemBoost;
+        [SerializeField]
+        private ParticleSystem _particleSystemLanding;
+        
+        private void JumpBoost()
+        {
+            _particleSystemBoost.Play();
+        }
+
+        private void JumpEnd()
+        {
+            _particleSystemLanding.Play();
+        }
+    }
+}
