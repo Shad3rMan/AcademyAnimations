d590c9be6c7f7931784849483f159a1ad72242fd
Di <diana.aristova.99@gmail.com>
2020-10-22 23:53:36 +0300

    Add effect.

---
+++ Assets/BoyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class BoyController : MonoBehaviour
+{
+     private void Effect()
+        {
+            var spiteRender = GetComponent<SpriteRenderer>();
+            var sequence = DOTween.Sequence();
+            
+            sequence.Append(spiteRender.DOColor(Color.red, 0.1f));
+            sequence.Append(transform.DOScale(Vector3.one * 2, 0.3f));
+            sequence.Append(transform.DOScale(Vector3.one, 0.3f));
+            sequence.OnComplete(() => spiteRender.DOColor(Color.white, 0.1f));
+        }
+}
