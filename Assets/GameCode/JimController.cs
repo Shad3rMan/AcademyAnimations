fe5f3e5f3a2f2e1f6c8093babfc9dcbf99567642
romanlu <romanlu@playtika.com>
2020-10-22 20:11:30 +0300

    Initial Jim commit

---
+++ Assets/GameCode/JimController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JimController : MonoBehaviour
+{
+    private Animator anim;
+
+    public bool isJumping;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !anim.GetBool("isJumping"))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        anim.SetBool("isJumping", !anim.GetBool("isJumping"));
+    }
+
+    public void EndJump()
+    {
+        Debug.Log("EndingJump");
+        anim.SetBool("isJumping", false);
+    }
+}
