384c5e40895f34e76ab4d5c2e39beba499570141
romanlu <romanlu@playtika.com>
2020-10-22 22:02:18 +0300

    updates

---
+++ Assets/GameCode/JimAnimationEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JimAnimationEvent : MonoBehaviour
+{
+    private JimController jimController;
+
+    private void Awake()
+    {
+        jimController = GetComponentInParent<JimController>();
+    }
+
+    private void JumpStop()
+    {
+        if (jimController != null)
+        {
+            jimController.Jump();
+        }
+    }
+}
--- Assets/GameCode/JimController.cs
+++ Assets/GameCode/JimController.cs
@@ -1,35 +1,37 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class JimController : MonoBehaviour
 {
+    [SerializeField] private float walkSpeed = 5f;
     private Animator anim;
-
-    public bool isJumping;
-
+    private SpriteRenderer spriteRenderer;
     private void Awake()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
     {
+        Move();
         if (Input.GetKeyDown(KeyCode.Space) && !anim.GetBool("isJumping"))
         {
             Jump();
         }
     }
 
-    private void Jump()
+    public void Jump()
     {
         anim.SetBool("isJumping", !anim.GetBool("isJumping"));
     }
 
-    public void EndJump()
+    private void Move()
     {
-        Debug.Log("EndingJump");
-        anim.SetBool("isJumping", false);
+        var input = Input.GetAxis("Horizontal");
+        var direction = new Vector2(input, transform.position.y);
+        anim.SetBool("isMoving", input != 0.0f);
+        spriteRenderer.flipX = (input < 0);
+        transform.Translate(direction * walkSpeed * Time.deltaTime);
     }
 }
