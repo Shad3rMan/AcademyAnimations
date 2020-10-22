162832bfcdb7e3eec86cf6bef1f2d47fb5984987
Di <diana.aristova.99@gmail.com>
2020-10-23 00:18:12 +0300

    Add fier.

--- Assets/GameCode/BoyController.cs
+++ Assets/GameCode/BoyController.cs
@@ -10,7 +10,7 @@ public class BoyController : MonoBehaviour
             var spiteRender = GetComponent<SpriteRenderer>();
             var sequence = DOTween.Sequence();
             
-            sequence.Append(spiteRender.DOColor(Color.red, 0.1f));
+            sequence.Append(spiteRender.DOColor(Color.green, 0.1f));
             sequence.Append(transform.DOScale(Vector3.one * 2, 0.3f));
             sequence.Append(transform.DOScale(Vector3.one, 0.3f));
             sequence.OnComplete(() => spiteRender.DOColor(Color.white, 0.1f));
