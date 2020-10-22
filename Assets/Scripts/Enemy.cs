1ef273e61515404444e6c7a5a2404a3584dec4e2
Maxim Kruk <maximkr@playtika.com>
2020-10-22 20:56:57 +0300

    QuestIsDone

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Vector3 _defaultPos;
+    [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private Sprite _deadSprite;
+    [SerializeField] private ParticleSystem _particle;
+    [SerializeField] private AudioClip _step;
+
+    [SerializeField] private GameObject _targetA;
+    [SerializeField] private GameObject _targetB;
+
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _sprite;
+
+    private bool isDead;
+    private bool isRight;
+    private int _health = 3;
+    void Start()
+    {
+        transform.position = _defaultPos;
+        _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if(!isDead)
+            CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        var dir = new Vector3(0, 0,0);
+        
+        if (isRight)
+            dir = new Vector3(1.0f, 0,0);
+        else 
+            dir = new Vector3(-1.0f, 0,0);
+        
+        transform.Translate(dir * _speed * Time.deltaTime);
+        CheckRight();
+    }
+
+    private void CheckRight()
+    {
+        if (transform.position.x <= _targetA.transform.position.x + 0.5f)
+        {
+            isRight = !isRight;
+            _sprite.flipX = true;
+        }
+        else if (transform.position.x >= _targetB.transform.position.x - 0.5f)
+        {
+            isRight = !isRight;
+            _sprite.flipX = false;
+        }
+    }
+
+    public void Damage(Vector3 dir)
+    {
+        _health--;
+        if (_health == 0)
+        {
+            isDead = true;
+            _animator.SetBool("Dead", isDead);
+            return;
+        }
+        _animator.SetBool("Damage", true);
+        _rigidbody.AddForce(dir * 2.0f);
+    }
+
+    private void EndDamage()
+    {
+        _animator.SetBool("Damage", false);
+    }
+
+    private void Dead()
+    {
+        _sprite.sprite = _deadSprite;
+    }
+    private void Step()
+    {
+        _particle.Play();
+        AudioSource.PlayClipAtPoint(_step, transform.position);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Vector3 _defaultPos;
+    [SerializeField] private float _speed = 4.0f;
+    [SerializeField] private float _highestJump = 2.0f;
+    [SerializeField] private LayerMask _raycastMask;
+    [SerializeField] private ParticleSystem _particle;
+    [SerializeField] private AudioClip _damage;
+    [SerializeField] private AudioClip _step;
+    [SerializeField] private AudioClip _swing;
+
+    private SpriteRenderer _sprite;
+    private Animator _animator;
+
+    private bool isAttack;
+    private bool isJump;
+    void Start()
+    {
+        transform.position = _defaultPos;
+        _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Update()
+    {
+        if (!isAttack)
+            CalculateMovement();
+        PlayAnimation();
+    }
+
+    private void CalculateMovement()
+    {
+        var hor = Input.GetAxis("Horizontal");
+        var dir = new Vector3(hor, 0, 0);
+        transform.Translate(dir * _speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        {
+            isJump = true;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * _highestJump,ForceMode2D.Impulse);
+        }
+        else if (Input.GetKeyDown(KeyCode.F) && !isJump)
+            isAttack = true;
+    }
+
+    private void PlayAnimation()
+    {
+        if (!isAttack)
+        {
+            if(!isJump)
+                _animator.SetBool("Move", Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A));
+            else
+                _animator.SetBool("Jump", isJump);
+            
+            if (Input.GetKey(KeyCode.D))
+                _sprite.flipX = true;
+            else if (Input.GetKey(KeyCode.A))
+                _sprite.flipX = false;
+            return;
+        }
+        _animator.Play("PlayerAttack");
+    }
+
+    private void Attack()
+    {
+        AudioSource.PlayClipAtPoint(_swing, transform.position);
+        var side = _sprite.flipX ? Vector2.right : Vector2.left;
+        var hit = Physics2D.Raycast(transform.position, side, 0.5f, _raycastMask);
+        if (hit.collider != null)
+        {
+            hit.collider.gameObject.GetComponent<Enemy>()?.Damage(side);
+            AudioSource.PlayClipAtPoint(_damage, transform.position);
+        }
+    }
+
+    private void EndOfAttacking()
+    {
+        isAttack = false;
+        _animator.Play("PlayerIdle");
+    }
+    private void EndOfJump()
+    {
+        isJump = false;
+        _animator.SetBool("Jump", isJump);
+    }
+
+    private void Step()
+    {
+        _particle.Play();
+        AudioSource.PlayClipAtPoint(_step, transform.position);
+    }
+}
