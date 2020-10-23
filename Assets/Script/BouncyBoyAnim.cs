5cb662dbfb2824f425181ab98ec2cdd969b1b822
dariak <dariak@playtika.com>
2020-10-23 10:37:45 +0300

    Add animation events

---
+++ Assets/Script/BouncyBoyAnim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BouncyBoyAnim : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particleSystem;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayParticles()
+    {
+        particleSystem.Play();
+    }
+
+    public void PlaySound()
+    {
+        _audioSource.PlayOneShot(_audioSource.clip);
+    }
+}
---
+++ Assets/Script/Jumping.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script
+{
+    [RequireComponent(typeof(Animation))]
+    public class Jumping : MonoBehaviour
+    {
+        private Animation _animation;
+        [SerializeField] private ParticleSystem _particleSystem;
+
+        private void Awake()
+        {
+            _animation = GetComponent<Animation>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _animation[_animation.clip.name].speed *= -1;
+            }
+        }
+
+        public void PlayParticles()
+        {
+            _particleSystem.Play();
+        }
+    }
+}
