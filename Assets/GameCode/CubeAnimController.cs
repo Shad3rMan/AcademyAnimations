e634743fd776feea61a64f0f1fe675533d53bcda
Marchenko Maxim <maximm@playtika.com>
2020-10-23 12:08:20 +0300

    Added swap animation

---
+++ Assets/GameCode/CubeAnimController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class CubeAnimController : MonoBehaviour
+{
+    private Animation _animation;
+
+    private void Awake()
+    {
+        _animation = GetComponent<Animation>();
+    }
+    
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+        _animation[_animation.clip.name].speed *= -1;
+    }
+}
