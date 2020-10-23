70b3b1aaba3901eda4058080ba5b16c87d9b9d90
Ekaterina Kazakevich <ekaterinaka@playtika.com>
2020-10-23 10:48:23 +0300

    Bounce animation

---
+++ Assets/Scripts/JumpingCubeView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class JumpingCubeView : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particleSystem;
+    private Animation _animation;
+
+    private void Awake()
+    {
+        _animation = GetComponent<Animation>();
+        _animation.Play();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animation[_animation.clip.name].speed *= -1;
+        }
+    }
+
+    public void BurstParticles()
+    {
+        var system = Instantiate(_particleSystem,
+            new Vector3(
+                transform.position.x,
+                transform.position.y - 0.4f,
+                transform.position.z),
+                Quaternion.identity);
+        system.Play();
+    }
+}
