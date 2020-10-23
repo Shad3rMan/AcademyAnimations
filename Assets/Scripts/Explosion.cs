fe24b57fa238903aa1e929d6a4418b532ca22ab2
Ekaterina Kazakevich <ekaterinaka@playtika.com>
2020-10-23 11:08:42 +0300

    Link explosion (why?)

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particleSystem;
+
+    private void PlayEffect()
+    {
+        _particleSystem.Play();
+    }
+}
