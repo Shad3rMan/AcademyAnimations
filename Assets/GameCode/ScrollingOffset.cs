adf57fb4679b9956164a0d1e2292b72c721de8aa
Andrew Chaiko <andrewc@playtika.com>
2020-12-08 18:19:05 +0300

    Prepare environment for analysis

---
+++ Assets/GameCode/ScrollingOffset.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ScrollingOffset : MonoBehaviour
+{
+    private Renderer _renderer;
+    private Vector3 _startPosition;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        _startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        var offset = _renderer.material.mainTextureOffset;
+        offset.y = 1 - Time.time % 1;
+        _renderer.material.mainTextureOffset = offset;
+        var position = transform.position;
+        position.y = _startPosition.y + Mathf.PingPong(Time.time, 1);
+        transform.position = position;
+    }
+}
