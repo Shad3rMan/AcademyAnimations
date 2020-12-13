e8cc8af4c04f8b133f40320315ad0e7e9dd978c7
nbarkun <nikitaba@playtika.com>
2020-12-13 17:41:56 +0300

    Task done

--- Assets/GameCode/ScrollingOffset.cs
+++ Assets/GameCode/ScrollingOffset.cs
@@ -6,20 +6,21 @@ namespace GameCode
     {
         private Renderer _renderer;
         private Vector3 _startPosition;
-        private Material _material;
-        private Transform _transform;
+        private Material _sharedMaterial;
 
         private void Awake()
         {
             _renderer = GetComponent<Renderer>();
+            _sharedMaterial = _renderer.sharedMaterial;
             _startPosition = transform.position;
         }
 
         private void Update()
         {
-            var offset = _renderer.material.mainTextureOffset;
-            offset.y = 1 - Time.time % 1;
-            _renderer.material.mainTextureOffset = offset;
+            var offset = _sharedMaterial.mainTextureOffset;
+            offset.y = 1 - Time.time;
+            _sharedMaterial.mainTextureOffset = offset;
+            
             var position = transform.position;
             position.y = _startPosition.y + Mathf.PingPong(Time.time, 1);
             transform.position = position;
