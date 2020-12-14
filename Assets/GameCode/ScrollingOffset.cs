6d58d839a2c950dc858d7f1fc060804c089ead57
romanlu <romanlu@playtika.com>
2020-12-14 13:41:05 +0300

    optimizing things

--- Assets/GameCode/ScrollingOffset.cs
+++ Assets/GameCode/ScrollingOffset.cs
@@ -4,7 +4,6 @@ namespace GameCode
 {
     public class ScrollingOffset : MonoBehaviour
     {
-        private Renderer _renderer;
         private Vector3 _startPosition;
         private Material _material;
         private Transform _transform;
@@ -13,15 +12,15 @@ namespace GameCode
 
         private void Awake()
         {
-            _renderer = GetComponent<Renderer>();
+            _material = GetComponent<Renderer>().sharedMaterial;
             _startPosition = transform.position;
-            _offset = _renderer.sharedMaterial.mainTextureOffset;
+            _offset = _material.mainTextureOffset;
         }
 
         private void Update()
         {
             _offset.y = 1 - Time.time % 1;
-            _renderer.sharedMaterial.mainTextureOffset = _offset;
+            _material.mainTextureOffset = _offset;
             _position = transform.position;
             _position.y = _startPosition.y + Mathf.PingPong(Time.time, 1);
             transform.position = _position;
