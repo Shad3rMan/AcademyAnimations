dbb74a8fa017e4160f27ec7fb6734ba3df80e5bb
Taras Makarevich <tarasm@playtika.com>
2020-12-15 08:50:47 +0300

    Use asset bundles.

---
+++ Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class BuildAssetBundles
+{
+    [MenuItem("Assets/Build Asset Bundles in Project")]
+    private static void BuildAssetBundlesInProject()
+    {
+        var assetBundleDirectory = Application.streamingAssetsPath;
+        
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+                                        BuildAssetBundleOptions.None,
+                                        BuildTarget.Android);
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+                                        BuildAssetBundleOptions.None,
+                                        BuildTarget.StandaloneOSX);
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+                                        BuildAssetBundleOptions.None,
+                                        BuildTarget.iOS);
+    }
+}
---
+++ Assets/GameCode/Gameplay/SimpleAssetBundleBasedSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class SimpleAssetBundleBasedSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private string _assetBundleName;
+    [SerializeField]
+    private string _prefabName;
+    
+    private void Start()
+    {
+        var assetBundle
+            = AssetBundle
+                .LoadFromFile(Path.Combine(Application.streamingAssetsPath, 
+                                           _assetBundleName));
+        
+        if (assetBundle == null) {
+            Debug.Log("Failed to load AssetBundle " + _assetBundleName);
+            return;
+        }
+        
+        var prefab = assetBundle.LoadAsset<GameObject>(_prefabName);
+        Instantiate(prefab, transform);
+    }
+}
